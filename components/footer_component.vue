d602d7d67fb8c0a559bec6f9d5dd13bee353ee7f
pretyflaco <kemal@ideasarelikeflames.com>
2023-07-28 15:02:39 +0300

    fix email - faqs

--- components/footer_component.vue
+++ components/footer_component.vue
@@ -38,7 +38,7 @@
                     &nbsp;
                     <a target="_blank" href=" https://github.com/nuraelb/AdoptingBitcoin">/GitHub</a> &nbsp;
                     <br>
-                    <a href="mailto:info@adoptingbitcoin.org">email if you need / info@adoptingbitcoin.org</a>
+                    <a href="mailto:capetown@adoptingbitcoin.org">email if you need / capetown@adoptingbitcoin.org</a>
 
                 </div>
 
